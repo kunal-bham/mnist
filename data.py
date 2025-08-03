31b00472e19574cd80b9bca893f4288958481459
Kunal Bham <kunal14202@gmail.com>
2025-08-03 14:13:33 -0400

    loading data

---
+++ data.py
@@ -0,0 +1,29 @@
+from torchvision import datasets, transforms
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Downloads to ./data by default
+mnist_train = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+mnist_test = datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
+
+def load_mnist(image_path, label_path):
+    # Load images
+    with open(image_path, 'rb') as imgf:
+        _, num_images, rows, cols = struct.unpack(">IIII", imgf.read(16))
+        images = np.frombuffer(imgf.read(), dtype=np.uint8).reshape(num_images, rows, cols)
+
+    # Load labels
+    with open(label_path, 'rb') as labf:
+        _, num_labels = struct.unpack(">II", labf.read(8))
+        labels = np.frombuffer(labf.read(), dtype=np.uint8)
+
+    # image is list of lists (28x28); label is singular number
+    return images, labels
+
+
+# Loading training sample
+images, labels = load_mnist(
+    'data/MNIST/raw/train-images-idx3-ubyte',
+    'data/MNIST/raw/train-labels-idx1-ubyte'
+)
